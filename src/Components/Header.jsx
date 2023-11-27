5e70354f14377644b59ca2bad526e908ff1a4f12
Tara Skoglund <skoglundtara@gmail.com>
2023-11-27 16:13:02 +0100

    header done

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import { Search } from "lucide-react";
+function Header() {
+  return (
+    <header className="font-serif w-screen h-14  bg-red-800 flex items-center gap-6 p-8">
+      <h1 className="text-3xl pb-2">Q</h1>
+      <label htmlFor="search" className="border-2 border-black rounded flex">
+        <Search className="text-sm p-1" />
+        <input
+          type="text"
+          placeholder=" Search"
+          className=" bg-red-800 text-black w-96 placeholder:text-black focus:outline-none"
+        />
+      </label>
+    </header>
+  );
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["Ariata Display", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+    },
     extend: {},
   },
   plugins: [],
