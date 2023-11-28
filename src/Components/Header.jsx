37c882154a68b4db4efe4d821968c1ba7b02454d
Tara Skoglund <skoglundtara@gmail.com>
2023-11-28 11:21:00 +0100

    add api and handleSearch

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,39 @@
+import axios from "axios";
 import { Search } from "lucide-react";
-function Header() {
+import React, { useState } from "react";
+
+function Header({ onSearchResult }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}${searchTerm}`);
+      // Anropa onSearchResult-funktionen för att skicka sökresultatet tillbaka till App-komponenten
+      onSearchResult(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      // Hantera fel här, t.ex. visa ett meddelande till användaren
+    }
+  };
+
   return (
-    <header className="font-serif w-screen h-14  bg-red-800 flex items-center gap-6 p-8">
+    <header className="font-serif w-screen h-14 bg-red-800 flex items-center gap-6 p-8">
       <h1 className="text-3xl pb-2">Q</h1>
-      <label htmlFor="search" className="border-2 border-black rounded flex">
-        <Search className="text-sm p-1" />
+      <label
+        htmlFor="search"
+        className="border-2 border-black rounded flex px-1"
+      >
         <input
           type="text"
-          placeholder="Search"
-          className=" bg-red-800 text-black w-96 placeholder:text-black focus:outline-none"
+          placeholder="What do you wanna know"
+          className="bg-red-800 text-black w-96 placeholder:text-black focus:outline-none p-1"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
+        <button onClick={handleSearch}>
+          <Search />
+        </button>
       </label>
     </header>
   );
