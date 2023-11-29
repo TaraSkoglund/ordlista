299194de3d8aaf3b8c6ade39f64c166fdcfa9085
Tara Skoglund <skoglundtara@gmail.com>
2023-11-29 12:20:48 +0100

    uppdate searchResult and send it to result

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import React, { useState } from "react";
 import Header from "./Components/Header";
 import Result from "./Components/Result";
 
 function App() {
+  const [searchResult, setSearchResult] = useState(null);
+
+  // Funktion för att uppdatera sökresultatet från API-anropet
+  const handleSearchResult = (result) => {
+    setSearchResult(result);
+  };
+
   return (
     <div>
-      <Header />
+      <Header onSearchResult={handleSearchResult} />
       <div className="w-full h-screen bg-red-800 p-12 flex justify-center gap-4">
-        <Result />
-        <Result />
+        {/* Skicka sökresultatet som en prop till Result */}
+        <Result searchResult={searchResult} />
       </div>
     </div>
   );
--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,13 +1,35 @@
-function Result() {
+import React from "react";
+
+function Result({ searchResult }) {
   return (
-    <div className="font-serif  max-w-md h-max border-2 border-black rounded p-2">
-      <h1 className="font-semibold text-xl pb-6">Word</h1>
-      <p className="pb-2">Sound;</p>
-      <p>
-        mening; Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-        nesciunt repellat totam est eveniet ullam non aperiam debitis animi
-        nobis exercitationem vero quaerat eos, quia iste beatae quis ut atque?
-      </p>
+    <div className="font-serif max-w-md h-max border-2 border-black rounded p-2">
+      {searchResult && (
+        <>
+          <h1 className="font-semibold text-xl pb-6">{searchResult[0].word}</h1>
+          {searchResult[0].phonetics && searchResult[0].phonetics[0].audio && (
+            <audio controls>
+              <source
+                src={searchResult[0].phonetics[0].audio}
+                type="audio/mpeg"
+              />
+              Your browser does not support the audio element.
+            </audio>
+          )}
+          {searchResult[0].meanings &&
+            searchResult[0].meanings.map((meaning, index) => (
+              <div key={index}>
+                <p className="pb-2">{meaning.partOfSpeech}</p>
+                {meaning.definitions &&
+                  meaning.definitions.map((definition, index) => (
+                    <div key={index}>
+                      <p>{definition.definition}</p>
+                      <p>Example: {definition.example}</p>
+                    </div>
+                  ))}
+              </div>
+            ))}
+        </>
+      )}
     </div>
   );
 }
