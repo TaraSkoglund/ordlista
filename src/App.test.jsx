388d0d2a9f0de0eb927361589a00979335eec004
Tara Skoglund <skoglundtara@gmail.com>
2023-11-30 16:34:25 +0100

    add some more test (wip)

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
-import { expect, it } from "vitest";
+import { expect, it, shallow, vi } from "vitest";
 // import Result from "./Components/Result";
+import Header from "./Components/Header";
 
 it("should search for a word", async () => {
   const searchTerm = "hello"; // Ord att söka efter
@@ -29,7 +30,54 @@ it("should display search result in a user-friendly form", async () => {
   // Rendera komponenten med det simulerade sökresultatet
   // const resultComponent = shallow(<Result searchResult={searchResult} />);
 
-  // // Förväntar att komponenten renderar ordet och dess definition
+  // Förväntar att komponenten renderar ordet och dess definition
   // expect(resultComponent.contains("hello")).toBe(true);
   // expect(resultComponent.contains("used as a greeting")).toBe(true);
+});
+
+it("should show an error message for empty search field", async () => {
+  const onSearchResultMock = vi.mock(); // Skapa en mock-funktion för onSearchResult
+
+  // Rendera Header-komponenten med en mockad onSearchResult-funktion
+  const headerComponent = shallow(
+    <Header onSearchResult={onSearchResultMock} />
+  );
+
+  // Simulera en sökning med ett tomt sökfält
+  headerComponent.find("button").simulate("click");
+
+  // Förvänta dig att ett felmeddelande visas
+  expect(
+    headerComponent.contains("Failed to fetch data. Please try again.")
+  ).toBe(true);
+});
+
+it("should play audio from the API if available", () => {
+  const searchResultWithAudio = {
+    word: "hello",
+    phonetics: [
+      {
+        text: "hə'ləʊ",
+        audio:
+          "https://ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3",
+      },
+    ],
+    meanings: [
+      {
+        partOfSpeech: "exclamation",
+        definitions: [{ definition: "used as a greeting" }],
+      },
+    ],
+  };
+
+  const headerComponent = shallow(<Header />);
+
+  // Uppdatera sökresultatet i komponenten
+  headerComponent.instance.setState({ searchResult: searchResultWithAudio });
+
+  // Förvänta dig att ljudkomponenten finns och innehåller en länk till en ljudfil
+  expect(headerComponent.find("audio").exists()).toBe(true);
+  expect(headerComponent.find("source").prop("src")).toBe(
+    searchResultWithAudio.phonetics[0].audio
+  );
 });
